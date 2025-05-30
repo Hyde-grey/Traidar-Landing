74c6eafd00b8ea1085394786c86cc04a0b2797a1
Hyde <francoiskhamsing@gmail.com>
2025-05-30 23:29:55 +0100

    feat: reworked landing page with new direction and mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { AnimatePresence } from "framer-motion";
 
 import Logo from "./components/Logo/Logo";
-import Title from "./components/Title/Title";
+import Title from "./components/Title/title";
 import Orb from "./components/Motion/Orb/Orb";
 
 import "./App.css";
